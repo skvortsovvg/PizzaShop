26bb255d8cc90b351c5ca8ba258fbf53a52582bf
Vovanches <vovanches@i.ua>
2016-09-27 16:59:09 +0300

    Create product table

--- db/migrate/20160927133256_product_name.rb
+++ db/migrate/20160927133256_product_name.rb
@@ -1,7 +1,11 @@
 class ProductName < ActiveRecord::Migration
   def change
   	create_table :products do |t|
-  		t.text :name
+  		t.string 	:title
+  		t.text 		:description
+  		t.decimal 	:price, :size
+  		t.boolean 	:is_spicy, :is_veg, :is_best_offer
+  		t.string 	:path_to_image
   		t.timestamps
   	end
   end
