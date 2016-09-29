7451f136d758db8426245dbcf70fc6cab6440529
Vovanches <vovanches@i.ua>
2016-09-29 16:30:38 +0300

    add_to_cart function

--- public/app.js
+++ public/app.js
@@ -2,6 +2,11 @@ function puts_something()
 {
 	var x = window.localStorage['aaa'];
 	x = +x+1 || 1 //if x = NaN (first call), then put 
-	window.localStorage['aaa'] = x
+	window.localStorage['aaa'] = x;
 	alert(x);
+}
+
+function add_to_cart(id)
+{
+	alert('Added ' + id + ' to cart!');
 }
